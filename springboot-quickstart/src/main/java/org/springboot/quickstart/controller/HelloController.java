c9c5da553096e9980d044a612164f493dbd8219e
bingyangxuesong <bingyangxuesong>
2018-05-04 16:28:12 +0800

    quick start demo

---
+++ springboot-quickstart/src/main/java/org/springboot/quickstart/controller/HelloController.java
@@ -0,0 +1,15 @@
+package org.springboot.quickstart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String sayHello() {
+		return "hello,spring boot!";
+	}
+}
